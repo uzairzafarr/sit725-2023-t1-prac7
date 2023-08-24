036b6bc4e0eea24adcd43fe06d0a28e14590caac
uzairzafarr <uzairrzafar1@gmail.com>
2023-08-24 20:36:14 +1000

    getworking
    Done changes to make DB works

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 let express = require("express");
 let app = express();
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://uzairrzafar1:Deakin3@cluster0.miausil.mongodb.net/?retryWrites=true&w=majority";
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://uzairrzafar1:Deakin3@cluster0.miausil.mongodb.net/?retryWrites=true&w=majority";
 
 let port = process.env.port || 3000;
 let collection;
 
 app.use(express.static(__dirname + "/"));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-
+app.use(express.urlencoded({ extended: false }));
 
 const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-  });
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 async function runDB() {
   try {
@@ -26,39 +26,42 @@ async function runDB() {
     collection = client.db().collection("Cat");
     console.log(collection);
   } catch (ex) {
-    console.error(ex);
+    console.error('Error'+ex);
   }
 }
+
+
 
 app.get("/", (req, res) => {
   res.render("index.html");
 });
 
-
-app.get('/api/cats', (req,res) => {
-    getAllCats((err,result)=>{
-        if (!err) {
-            res.json({statusCode:200, data:result, message:'Get cats successfully'});
-        }
-    });
+app.get("/api/cat", (req, res) => {
+  getAllCats((err, result) => {
+    if (!err) {
+      res.json({
+        statusCode: 200,
+        data: result,
+        message: "Get cats successfully",
+      });
+    }
+  });
 });
-app.post('/api/cat', (req,res)=>{
-    let cat = req.body;
-    postCat(cat, (err, result) => {
-        if (!err) {
-            res.json({statusCode:201, data:result, message:'Successfull'});
-        }
-    });
+app.post("/api/cat", (req, res) => {
+  let cat = req.body;
+  postCat(cat, (err, result) => {
+    if (!err) {
+      res.json({ statusCode: 201, data: result, message: "Successfull" });
+    }
+  });
 });
 
-function postCat(cat,callback) {
-    collection.insertOne(cat,callback);
+function postCat(cat, callback) {
+  collection.insertOne(cat, callback);
 }
 
-
-
-function getAllCats(callback){
-    collection.find({}).toArray(callback);
+function getAllCats(callback) {
+  collection.find({}).toArray(callback);
 }
 
 app.listen(port, () => {
